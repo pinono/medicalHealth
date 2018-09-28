53601b48db4dd49f8b01287763e8d6d05ce08616
unknown <>
2018-09-28 19:21:51 +0800

    恢复原配置

--- build/utils.js
+++ build/utils.js
@@ -53,7 +53,6 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        publicPath : '../../',
         fallback: 'vue-style-loader'
       })
     } else {
